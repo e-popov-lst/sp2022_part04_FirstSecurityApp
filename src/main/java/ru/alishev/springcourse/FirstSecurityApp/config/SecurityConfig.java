6b9c9130cd622c434c5ed8cdc1d653e626ef035f
Eugene Popov <eugene.popov@list.ru>
2023-05-28 20:38:34 +0300

    lesson 12-80

--- src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/alishev/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -1,23 +1,34 @@
 package ru.alishev.springcourse.FirstSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.alishev.springcourse.FirstSecurityApp.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.alishev.springcourse.FirstSecurityApp.services.PersonDetailService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // WebSecurityConfigurerAdapter is depricated
-    private final AuthProviderImpl authProvider;
+
+
+    private final PersonDetailService personDetailService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailService personDetailService) {
+        this.personDetailService = personDetailService;
     }
 
     // Настраивает аутентификацию
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailService);
+    }
+
+    // указываем каким алгоритм будем шифровать пароль
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();  // никак не шифруем (пока)
     }
 }
